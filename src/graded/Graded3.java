8ff051d9267aa10bbecb94a82154bc6a4e587302
dctisdal <49253686+dctisdal@users.noreply.github.com>
2020-08-03 21:50:10 +0700

    first commit

---
+++ src/graded/Graded3.java
@@ -0,0 +1,12 @@
+package graded;
+
+	import java.util.logging.Level;
+	import java.util.logging.Logger;
+
+public class Graded3 {
+	public static void main(String[] args) {
+		// prints "Hello World" to terminal window
+		Logger logger = Logger.getLogger(Graded3.class.getName());
+		logger.log(Level.INFO, "Hello World!");
+	}
+}
